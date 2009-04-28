f5bb9c673d91db9014dfffff040c7b99ed65df3e
Changwoo Ryu <cwryu@debian.org>
2009-04-29 03:15:40 +0900

    Add the proposal form validation on word and pos

--- galkwi/forms.py
+++ galkwi/forms.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from django import forms
+from django.forms.util import ValidationError
 from galkwi.models import *
 from django.contrib.auth.models import Permission
+from django.utils.translation import ugettext_lazy as _
 
 class UserRegistrationForm(forms.Form):
     agree = forms.BooleanField(label=u'위 사항에 동의합니다')
@@ -17,7 +19,23 @@ class ProposalVoteForm(forms.ModelForm):
 class ProposalCancelForm(forms.Form):
     pass
 
+error_messages = {
+    'required': _(u'This field is required.'),
+    'invalid': _(u'Enter a valid value.'),
+}
+
 class ProposalEditAddForm(forms.ModelForm):
+    def clean_word(self):
+        word = self.cleaned_data.get('word')
+        if not word:
+            raise ValidationError(error_messages['required'])
+        return word
+    def clean_pos(self):
+        pos = self.cleaned_data.get('pos')
+        if not pos:
+            raise ValidationError(error_messages['required'])
+        return pos
+        
     class Meta:
         model = Proposal
         exclude = ['word_substrings',
@@ -30,6 +48,17 @@ class ProposalEditRemoveForm(forms.ModelForm):
         fields = ['rationale']
 
 class ProposalEditUpdateForm(forms.ModelForm):
+    def clean_word(self):
+        word = self.cleaned_data.get('word')
+        if not word:
+            raise ValidationError(error_messages['required'])
+        return word
+    def clean_pos(self):
+        pos = self.cleaned_data.get('pos')
+        if not pos:
+            raise ValidationError(error_messages['required'])
+        return pos
+        
     class Meta:
         model = Proposal
         exclude = ['word_substrings',
