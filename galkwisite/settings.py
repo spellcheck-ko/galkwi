46282210a70ad94b9436bc3b6a3d351a16dedd6e
Changwoo Ryu <cwryu@debian.org>
2017-02-15 17:05:30 +0900

    facebook & github login

--- galkwisite/settings.py
+++ galkwisite/settings.py
@@ -74,8 +74,8 @@ TEMPLATES = [
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
-    # 'social_core.backends.facebook.FacebookOAuth2',
-    # 'social_core.backends.github.GithubOAuth2',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.github.GithubOAuth2',
     'social_core.backends.google.GoogleOAuth2',
     # 'social_core.backends.open_id.OpenIdAuth',
     # 'social_core.backends.twitter.TwitterOAuth',
@@ -133,6 +133,14 @@ SOCIAL_AUTH_PIPELINE = (
 )
 
 # Google Keys
+
+SOCIAL_AUTH_FACEBOOK_KEY = ''
+SOCIAL_AUTH_FACEBOOK_SECRET = ''
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {'fields': 'id,name,email'}
+
+SOCIAL_AUTH_GITHUB_KEY = ''
+SOCIAL_AUTH_GITHUB_SECRET = ''
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = ''
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = ''
