6673618b3758b39381de3dae1c690f116569b652
Changwoo Ryu <cwryu@debian.org>
2017-02-19 02:00:51 +0900

    export 포맷을 간략한 json으로 변경
    hunspell-dict-ko에 사용하는 포맷을 작게 하려고 꼭 필요한 단어, 품사, 속성,
    어근 정도만 남겨두고 생략.
    
    입력한 데이터를 (설명 포함해) 충실히 표현하면 백업과 복구 포맷으로 의미가 있을
    수 있으나, 그 데이터를 사용하지 않는 hunspell-dict-ko에 포함할 이유는 없다.
    백업과 복구는 dumpdata/loaddata django manager 명령으로 대신할 수 있을 것이다.

--- galkwiapp/management/commands/dict_export.py
+++ galkwiapp/management/commands/dict_export.py
@@ -1,11 +1,14 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 from galkwiapp.models import Entry
-from xml.sax.saxutils import escape as xml_escape
+import json
 
+
+def json_str(s):
+    return ('%s' % json.dumps(s, ensure_ascii=False))
 
 class Command(BaseCommand):
-    help = 'Export XML word data'
+    help = 'Export JSON word data'
 
     def add_arguments(self, parser):
         parser.add_argument('filename', type=str)
@@ -15,33 +18,42 @@ class Command(BaseCommand):
         self.do_export(open(filename, 'w'))
 
     def do_export(self, file):
+        contributors_set = set()
+
+        def write_entry(file, entry):
+            word = entry.latest.word
+            file.write('{\n')
+            file.write(' "word":%s,\n' % json_str(word.word))
+            file.write(' "pos":%s' % json_str(word.pos))
+            if word.props:
+                file.write(',\n')
+                props = word.props.split(',')
+                props_json = ','.join([json_str(s) for s in props])
+                file.write(' "props": [%s]' % props_json)
+            if word.stem:
+                file.write(',\n')
+                file.write(' "stem":%s' % json_str(word.stem))
+            file.write('\n}')
+
+            rev = entry.latest
+            while rev:
+                contributors_set.add(rev.user.username)
+                contributors_set.add(rev.reviewer.username)
+                rev = rev.parent
+
         entries = Entry.objects.filter(latest__deleted=False)
         entries = entries.order_by('latest__word__word')
-        file.write('<galkwi-exported version="2.0">\n')
-        for entry in entries:
-            rev = entry.latest
-            word = rev.word
-            file.write('<Entry>\n')
-            file.write(' <word>%s</word>\n' % xml_escape(word.word))
-            file.write(' <pos>%s</pos>\n' % xml_escape(word.pos))
-            if word.props:
-                file.write(' <props>%s</props>\n' % xml_escape(word.props))
-            if word.stem:
-                file.write(' <stem>%s</stem>\n' % xml_escape(word.stem))
-            if word.etym:
-                file.write(' <etym>%s</etym>\n' % xml_escape(word.etym))
-            if word.description:
-                file.write(' <description>%s</description>\n' % xml_escape(word.description))
-            file.write(' <revisions>\n')
-            while rev:
-                file.write('  <revision>\n')
-                file.write('   <name>%s</name>\n' % xml_escape(rev.user.username))
-                timestamp = rev.timestamp.astimezone(timezone.utc)
-                file.write('   <datetime>%s</datetime>\n' % timestamp.strftime('%Y-%m-%dT%H:%M:%SZ'))
-                if rev.comment:
-                    file.write('  <comment>%s</comment>\n' % xml_escape(rev.comment))
-                file.write('  </revision>\n')
-                rev = rev.parent
-            file.write(' </revisions>\n')
-            file.write('</Entry>\n')
-        file.write('</galkwi-exported>\n')
+        file.write('{\n')
+        file.write('"entries":[\n')
+        if len(entries) > 0:
+            write_entry(file, entries[0])
+            for entry in entries[1:]:
+                file.write(',\n')
+                write_entry(file, entry)
+            file.write('\n')
+        file.write('],\n')
+        file.write('"contributors":[\n')
+        contributors = sorted(list(contributors_set))
+        file.write(' ' + ',\n '.join([json_str(c) for c in contributors]) + '\n')
+        file.write(']\n')
+        file.write('}\n')
